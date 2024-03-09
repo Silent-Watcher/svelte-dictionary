bfe9655d3b2ff5b57777f59b1098dba31bfe2c6a
Ali t.nazari <alitabatabaee20@gmail.com>
2024-03-09 20:33:08 +0330

    add github project link

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,7 @@
 
 <h1 class="text-center dark:text-white text-7xl my-10">Dictionary</h1>
 <section class="w-full md:w-1/2 md:mx-auto flex content-center items-center text-center">
-          <a href="#" target="_blank" class="btn"><GithubSolid size='lg'/></a>
+          <a href="https://github.com/Silent-Watcher/svelte-dictionary" target="_blank" class="btn"><GithubSolid size='lg'/></a>
           <DarkMode class="btn" size="lg"/>
 </section>
 <Header/>
